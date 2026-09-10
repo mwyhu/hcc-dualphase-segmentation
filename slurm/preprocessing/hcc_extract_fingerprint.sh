79bc7c8b8c8c02d0c737ee73fc307bb8cdd863c1
Michelle Hu <mhu3@int6.local.snellius.surf.nl>
2026-09-10 09:46:31 +0200

    HCC extract fingerprint test

---
+++ slurm/preprocessing/hcc_extract_fingerprint.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --partition=genoa
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=128G
+#SBATCH --time=04:00:00
+#SBATCH --job-name="extract_fingerprint_HCC"
+#SBATCH --output=logs/%x_%j.out
+#SBATCH --error=logs/%x_%j.err
+
+
+cd /projects/prjs2180/code/hcc-dualphase-segmentation
+
+source setup_env.sh
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+echo "Job started"
+echo "Date and time:"
+date
+echo "Node: $SLURMD_NODENAME"
+echo "Job ID: $SLUR_JOB_ID"
+
+
+
+nnUNetv2_extract_fingerprint \
+    -d 208 \
+    --verify_dataset_integrity
+
+
+
+
+echo "Job finished"
+echo "Date and time:"
+date
