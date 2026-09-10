2e2d19f21a9e308e15d38a565287946b4199cb3b
Michelle Hu <m.w.hu2@student.vu.nl>
2026-09-10 14:26:28 +0200

    Extract fingerprint for ceiling dataset

---
+++ slurm/preprocessing/ceiling_extract_fingerprint.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --partition=genoa
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=128G
+#SBATCH --time=02:00:00
+#SBATCH --job-name="extract_fingerprint_ceiling"
+#SBATCH --output=logs/%x_%j.out
+#SBATCH --error=logs/%x_%j.err
+
+
+cd /projects/prjs2180/code/hcc-dualphase-segmentation
+
+source setup_env.sh
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+echo "Job started"
+echo "Date and time:"
+date
+echo "Node: $SLURMD_NODENAME"
+echo "Job ID: $SLUR_JOB_ID"
+
+
+nnUNetv2_extract_fingerprint \
+    -d 003 \
+    --verify_dataset_integrity
+
+
+echo "Job finished"
+echo "Date and time:"
+date
