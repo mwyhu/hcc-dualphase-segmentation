ae56dbb10b45e43b8d0c0f25d09ed5cf7ce46cdd
Michelle Hu <m.w.hu2@student.vu.nl>
2026-09-10 16:24:32 +0200

    Slurms for fold 0 training, TS plans + random/pretrained weights for SP and DP

---
+++ slurm/training/TS_DP.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#SBATCH --job-name=TS_DP
+#SBATCH --account=prjs2180
+#SBATCH --partition=gpu_a100
+#SBATCH --gpus=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=128G
+#SBATCH --time=60:00:00
+#SBATCH --output=logs/%x_%j.out
+#SBATCH --error=logs/%x_%j.err
+
+set -euo pipefail
+
+cd /projects/prjs2180/code/hcc-dualphase-segmentation
+
+source setup_env.sh
+
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export nnUNet_n_proc_DA=$SLURM_CPUS_PER_TASK
+
+echo "Job started"
+echo "Date and time:"
+date
+echo "Node: $SLURMD_NODENAME"
+echo "Job ID: $SLURM_JOB_ID"
+
+export nnUNet_results=/projects/prjs2180/data/nnUNet_results/TS_DP
+
+nnUNetv2_train \
+    1 \
+    3d_fullres \
+    0 \
+    -tr nnUNetTrainer \
+    -p TSLL_DP_plans \
+    --npz
+
+echo "Job finished"
+echo "Date and time:"
+date
