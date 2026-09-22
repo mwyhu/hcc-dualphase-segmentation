c33c4cadfdeec2611f3553464509136987017cfe
Michelle Hu <m.w.hu2@student.vu.nl>
2026-09-22 08:58:54 +0200

    Reduce SP validation slurm to single fold

--- slurm/training/validation/SP_1e3_poly.sh
+++ slurm/training/validation/SP_1e3_poly.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=SP_poly_1e3
+#SBATCH --partition=genoa
 #SBATCH --partition=gpu_a100
 #SBATCH --gpus=1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=128G
-#SBATCH --time=60:00:00
+#SBATCH --cpus-per-task=16
+#SBATCH --time=20:00:00
 #SBATCH --array=1-4
 #SBATCH --output=logs/%x_fold%A_%a.out
 #SBATCH --error=logs/%x_fold%A_%a.err
@@ -31,7 +31,6 @@ nnUNetv2_train \
     "$FOLD" \
     -tr nnUNetTrainer \
     -p TSLL_SP_plans \
-    -pretrained_weights /projects/prjs2180/pretrained_models/TotalSegmentator/591_liver_lesions/checkpoint_final.pth \
     --npz \
     --val
 
